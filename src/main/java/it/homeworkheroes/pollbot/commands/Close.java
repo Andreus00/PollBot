17fb078475451f39059ba53cc437d2965e7e3916
Andreus00 <sanchietti.1883210@studenti.uniroma1.it>
2021-11-10 20:20:31 +0100

    Minor changes

--- src/main/java/it/homeworkheroes/pollbot/commands/Close.java
+++ src/main/java/it/homeworkheroes/pollbot/commands/Close.java
@@ -5,6 +5,7 @@ import it.homeworkheroes.pollbot.classes.EMONUMBER;
 import it.homeworkheroes.pollbot.classes.Poll;
 
 import java.util.StringTokenizer;
+import java.util.stream.Collectors;
 
 public class Close extends PollCommand{
 
@@ -19,11 +20,9 @@ public class Close extends PollCommand{
         Poll p = PollBotAPP.getPollList().getOrDefault(Long.valueOf(pollId), null);
 
         if (!checkChannel(p)) return;
-
         Poll.Option o = p.getOptionList().stream().max(Poll.Option::compareTo).orElse(null);
         if (o == null) return;
         p.close(EMONUMBER.values()[p.getOptionList().indexOf(o)].getEmoji());
-
     }
 
     @Override
